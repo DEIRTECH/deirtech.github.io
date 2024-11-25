210e98d659c7658b7fba40b1e3763b068766cf16
Feras Deiratany <fdeiratany@level20.com>
2024-11-24 20:37:50 -0500

    Switch to vite (#6)
    * init vite
    
    * clean up landing page

---
+++ marketing/src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Deirtech Consulting</h1>
+      <p className="read-the-docs">coming soon...</p>
+    </>
+  );
+}
+
+export default App;
